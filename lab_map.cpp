97767dc41ada31a1ed585f63b35685b5ae2f41dc
Lavlinsky-nikita <72300435+Lavlinsky-nikita@users.noreply.github.com>
2021-01-15 19:40:43 +0300

    комментарий

---
+++ lab_map.cpp
@@ -0,0 +1,39 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	//для корректного отображения кириллицы
+	setlocale(0, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	map<string, int> all_words; //словарь [слово, кол-во слов]
+
+	char text[1000];
+	cin.getline(text, sizeof(text)); // получили весь текст из консоли
+
+	char seps[] = " ,\t\n"; // разделители, по которым весь текст делится на слова
+	char* remained_text = NULL; // весь текст кроме текущего слова
+	char* cur_word = strtok_s(text, seps, &remained_text); // по разделителям делим текст на массив слов. Первое слово возвращаем, остаток текста записывается в remained_text
+
+	int words_count = 0; // всего слов
+	while (cur_word != NULL) // пока есть слово
+	{
+		all_words[cur_word]++; // по ключу, равному текущему слову, увеличиваем кол-во этих слов на 1
+		words_count++;
+
+		cur_word = strtok_s(NULL, seps, &remained_text); // берем следующее слово из текста
+	}
+	cout << "\n\n";
+
+	for (auto& word : all_words)
+	{
+		cout << word.second << "  " << word.first << " (" << setprecision(2) << (double)word.second / words_count * 100 << "%)" << endl;
+	}
+}
---
+++ lab_set.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <ctime>
+
+using namespace std;
+
+class pupil
+{
+public:
+
+	pupil(string s, int m)
+	{
+		account = s;
+		mark = m;
+	}
+
+	string account;
+	int mark;
+
+	friend ostream& operator << (ostream& os, pupil p)
+	{
+		os << p.account << ": " << p.mark;
+
+		return os;
+	}
+
+	//перегрузка оператора == для корректного сравнения двух Pupil
+	bool operator == (const pupil& p) const
+	{
+		return p.account == account;
+	}
+};
+
+// структура для собственного хэша. Одинаковые аккаунты будут давать одинаковый хэш => множество сданных тестов с одного аккаунта не будут засчитаны, только 1ый тест
+struct pupil_hash
+{
+	size_t operator()(const pupil& v) const
+	{
+		return hash<string>{}(v.account); // возвращает хэш на основе строки. Используется стандартная функция hash
+	}
+};
+
+int main()
+{
+	// указываем тип, который будет храниться в set и структуру pupil_hash, которая будет использоваться для сравнения объектов
+	unordered_set<pupil, pupil_hash> pupils;
+
+	srand(time(0));
+
+	int n = 10;
+
+	cout << "All pupils attempts:\n\n";
+	for (int i = 0; i < n; i++)
+	{
+		pupil p("account " + to_string(1 + rand() % 10), rand() % 101);
+
+		cout << p << endl;
+
+		pupils.insert(p);
+	}
+	cout << "\nAccepted attempts:\n";
+	for (auto& i : pupils) cout << i << endl;
+
+	return 0;
+}
